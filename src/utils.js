df020d8c4c28e3a4282bd5e2fd2e17f25b0ca786
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-10-20 22:37:04 +0700

    feat: checkpoint. everything is working the way it's intended to. no absurd delay on php

--- src/utils.js
+++ src/utils.js
@@ -5,32 +5,22 @@ function GET_API(apiLoc, callbackfn) {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", apiLoc, true);
   console.log("ready");
-  xhr.onload = (e) => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        console.log(xhr.responseText);
-        callbackfn(xhr.status, xhr.responseText);
-      } else {
-        console.error(xhr.statusText);
-      }
-    }
-  };
+
   xhr.onerror = (e) => {
     console.error(xhr.statusText);
+    callbackfn(500, xhr.statusText);
   };
   xhr.ontimeout = () => {
     console.error(`The request timed out.`);
+    callbackfn(500, "The request timed out.");
   };
   xhr.timeout = 10000;
-  // xhr.onreadystatechange = function () {
-  //   console.log("onreadystatechange", xhr.readyState);
-  //   if (xhr.readyState == 4) {
-  //     console.log("xhr.status", xhr.status);
-  //     if (xhr.status === 200) {
-  //       callbackfn(xhr.status, xhr.responseText);
-  //     }
-  //   }
-  // };
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      callbackfn(xhr.status, xhr.responseText);
+    }
+  };
   xhr.send();
 }
 
