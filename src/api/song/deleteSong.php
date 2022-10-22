ccc2dd2822d652b1ff39bed3f3e2fc5596abe2b2
addinnabilal <16520342@std.stei.itb.ac.id>
2022-10-23 00:20:58 +0700

    feat: add deleteSong api

---
+++ src/api/song/deleteSong.php
@@ -0,0 +1,45 @@
+<?php
+    require_once '../../global.php';
+
+    $map = backendConnection();
+    $conn = $map['conn'];
+    
+    if ($map['err'] != null) {
+        $conn->close();
+        exitWithError(500, $map['err']);
+    }
+
+    // check if user is admin
+    // $auth = checkIsAuthTokenValid();
+    // if (!$auth['is_admin']){
+    //     $conn->close();
+    //     exitWithError(401, "You are not authorized to delete song");
+    // }
+
+    if (empty($_REQUEST["song_id"])) {
+        exitWithError(400, "Song id unspecified");
+    }
+    $song_id = intval($_REQUEST["song_id"]);
+
+    // connect to database
+    $map = backendConnection();
+    $conn = $map['conn'];
+    if ($map['err'] != null) {
+        $conn->close();
+        exitWithError(500, $map['err']);
+    }
+
+    $stmt = $conn->prepare("DELETE FROM Song WHERE song_id = ?");
+    $stmt->bind_param("i", $song_id);
+
+    if($stmt->execute()){
+        $data = array(
+            "song_id" => $song_id
+        );
+        $conn->close();
+        exitWithDataReturned($data);
+    } else {
+        $conn->close();
+        exitWithError(500, "Error while deleting song");
+    }
+?>
